8f74d86a0a3fa3364423b29779c955c50663a637
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-08-24 14:52:56 +1000

    first stage of working search bar

--- src/components/Home.jsx
+++ src/components/Home.jsx
@@ -1,9 +1,13 @@
 import React from 'react'
+import SearchPlants from './SearchPlants'
+
 
 const Home = () => {
   return (
     <>
       <h1>My Garden Overview</h1>
+      <SearchPlants />
+
     </>
   )
 }
