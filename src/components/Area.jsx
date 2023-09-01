5a5dd9f237f2b8afb4e68c30e0c10f82994e996f
kalfung <14322@coderacademy.edu.au>
2023-09-01 14:40:02 +1000

    fixed up the filter in Area component and changed 'class' in AltNavBar to 'className'

--- src/components/Area.jsx
+++ src/components/Area.jsx
@@ -1,19 +1,24 @@
-import React from 'react'
+import React, { useState } from 'react'
 import {upperCaser} from './helperfuncs.js'
 import PlantList from './PlantList.jsx'
 
 const styles = { maxWidth: '540px', color: 'grey'}
 
 const Area = ({area, plants}) => {
+  console.log(plants)
   console.log(area)
   console.log(area.plants)
+  // could also do a use effect where the filtering will take place (i.e. wrapping the filter in a useEffect )
+  // set the result of the filter to state
+  // pass the state into plantlist
+  const areaPlants = plants.filter(plant => area.plants.includes(plant.id))
+  console.log(areaPlants)
+  const [active, setActive] = useState(false)
 
   // const areaPlantsIds = area.plants.map((plant) => plant.plantId)
   // console.log(areaPlantsIds)
   // const areaPlants = plants.filter(plant => areaPlantsIds.includes(plant.id))
   // // const areaPlants = plants.filter(plant => plant.id === area.plants.plantId)
-  // console.log(areaPlants)
-  // console.log(area.plants)
   return <>
     <div>Area.jsx</div>
     <div className="card mb-3" style={styles}>
@@ -28,8 +33,9 @@ const Area = ({area, plants}) => {
 
         <p className="card-text">{area.notes}</p>
       </div>
-      <PlantList plants={area.plants}/>
+      <PlantList plants={areaPlants} setActive={setActive}   />
     </div>
+    {active}
   </div>
 </div>
 
