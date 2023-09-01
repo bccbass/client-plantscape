4e5e032cba1845f4ed8f0a0c6f933cfda18ef7d1
kalfung <14322@coderacademy.edu.au>
2023-09-01 12:02:09 +1000

    Added Space component

---
+++ src/components/Space.jsx
@@ -0,0 +1,30 @@
+import React from 'react'
+import {upperCaser} from './helperfuncs.js'
+import AreaList from './AreaList.jsx'
+
+const styles = { maxWidth: '540px', color: 'grey'}
+
+const Space = ({space, plants}) => {
+  return <>
+    <div>Space.jsx</div>
+    <div className="card mb-3" style={styles}>
+    <div className="row g-0">
+      <div className="col-md-4">
+        <img src={space.imgUrl} className="img-fluid rounded-start" alt="image of selected space goes here"/>
+      </div>
+      <div className="col-md-8">
+        <div className="card-body">
+          <h5 className="card-title">{upperCaser(space.name)}</h5>
+          {/* <p className="card-text"><small className="text-body-secondary">{plant.scientific_name}</small></p> */}
+
+          <p className="card-text">{space.notes}</p>
+        </div>
+      <AreaList areas={space.areas} plants={plants}/>
+    </div>
+  </div>
+</div>
+
+</>
+}
+
+export default Space
