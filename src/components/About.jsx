c728f3f2ee5cef19297d183999dbbaab80918103
joshuadavis1990 <joshuadavis1990@me.com>
2023-09-02 19:48:20 +1000

    Added basic info to About page

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -5,7 +5,8 @@ function About() {
   return (
     <>
     <Title />
-    <p className="about">This is a paragraph about the app.</p>
+    <p className="about">We designed Plantscape to help you take better care of your garden.</p>
+    <p className="about">Start by adding a new Space, then assign an Area as well as plants. The app provides detailed information about your plants to help you take care of them.</p>
     </>
   )
 }
