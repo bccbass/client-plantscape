af45477c3ef78157566da6c8557b2ab7bb4c6b63
Benjamin Campbell <benjamincampbell@benjamins-mini.lan>
2023-09-03 19:41:10 +1000

    commented components with heavier logic

--- src/components/NewArea/AddPlants.jsx
+++ src/components/NewArea/AddPlants.jsx
@@ -4,7 +4,7 @@ import SearchPlants from "../Search/SearchPlants.jsx";
 import { upperCaser, updateUser } from "../helperfuncs.js";
 import "./AddPlants.css";
 
-const AddPlants = ({ user, setUser, area, space }) => {
+const AddPlants = ({ user, setUser, area }) => {
   const navigate = useNavigate();
   const [querySelection, setQuerySelection] = useState();
   const [thisArea, setThisArea] = useState();
@@ -12,44 +12,67 @@ const AddPlants = ({ user, setUser, area, space }) => {
   const [searchResults, setSearchResults] = useState();
   // const [active, setActive] = useState()
 
+  // add to "new plants array" which is a holding area of selected plants
+  // before committing them to an area and the global user.plants list
   useEffect(() => {
     if (querySelection && !newPlants.includes(querySelection)) {
       setNewPlants([...newPlants, querySelection]);
     }
   }, [querySelection]);
 
-  useEffect(() => {
-    let spaceFromUser =
-      user?.spaces && user.spaces.filter((el) => el.name == area.space)[0];
-    let areaFromUser =
-      spaceFromUser?.areas &&
-      spaceFromUser.areas.filter((el) => el.name == area.name)[0];
-    setThisArea(areaFromUser);
-  }, [user]);
+  // Locates users areas and spaces from user object using passed in area object
+  // These components should probably be refactored to use useContext/useReducer as the prop passing is unwieldly
 
+  // useEffect(() => {
+  //   // checks if user spaces exists and locates corresponding space from user object
+  //   // sets to variable spaceFromUser
+  //   let spaceFromUser =
+  //     user?.spaces && user.spaces.filter((el) => el.name == area.space)[0];
+  //   // checks if user areas exists and locates corresponding area from user object
+  //   // sets to variable areFromUser
+  //   let areaFromUser =
+  //     spaceFromUser?.areas &&
+  //     spaceFromUser.areas.filter((el) => el.name == area.name)[0];
+  //   // stores located user area in stateful object thisArea
+  //   setThisArea(areaFromUser);
+  // }, [user]);
+
+  // Adds selected plants to appropriate area and to user plants. U
+  // Updates the User in the database
   const handleAddPlantsClick = () => {
+    // create array of plant ids to store in user.plants and user.spaces.areas.plants
     let newPlantsIdArray = newPlants.map((el) => el.id);
+    // checks if user spaces exists and locates corresponding space from user object
+    // sets to variable spaceFromUser
     let spaceFromUser =
       user?.spaces && user.spaces.filter((el) => el.name == area.space)[0];
+    // checks if user areas exists and locates corresponding area from user object
     let areaFromUser =
       spaceFromUser?.areas &&
       spaceFromUser.areas.filter((el) => el.name == area.name)[0];
+    // sets to variable areFromUser
     setThisArea(areaFromUser);
+    // Assigns area plants to array of ID
     areaFromUser.plants = newPlantsIdArray;
+    // Set updated user object
+    // create set of unique ids in user.plants global plants array
     setUser(
       (user.plants = [...new Set([...user.plants, ...newPlantsIdArray])])
     );
-
+    // setUser to updated obj
     setUser(user);
+    // update user in the database
     updateUser(user);
+    // navigate to home page
     navigate("/");
   };
 
+  // Removes plant from New Plants array with click
   const handleDeletePlant = (e) => {
     const removeTargetPlant = newPlants.filter(
       (el) => el.common_name != e.target.value
     );
-    setNewPlants(removeTargetPlant); // delete
+    setNewPlants(removeTargetPlant);
   };
 
   return (
--- src/components/getAPI.js
+++ src/components/getAPI.js
@@ -1,7 +1,6 @@
 const devAPI = import.meta.env.VITE_API_HOST + import.meta.env.VITE_PORT
 const prodAPI = import.meta.env.VITE_API_HOST
 
-// Logic to determine what environment app is running in (development or production)
 const getAPIUrl = () => {
   if (import.meta.env.DEV) {
   return devAPI
@@ -9,7 +8,6 @@ const getAPIUrl = () => {
     return prodAPI}
   }
   
-// store API url in variable apiURL and export url for use throughout app
 const apiURL = getAPIUrl()
 
 export default apiURL
