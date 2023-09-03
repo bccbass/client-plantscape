2123c13d7074f1750c7508ca0716c3161e7e29ad
kalfung <14322@coderacademy.edu.au>
2023-09-03 20:22:50 +1000

    comments added to SpaceSelection component

--- src/components/SpaceSelection.jsx
+++ src/components/SpaceSelection.jsx
@@ -8,16 +8,13 @@ const style = {
   maxWidth: "900px"
 }
 
-// 
+// Parses in the user object and plants array to display the user's list of spaces
+// Passes in the user's spaces and plants array to the SpaceList component
 const SpaceSelection = ({  user, plants }) => {
   return (
     <>
       <h1>{user.firstName}'s Spaces</h1>
       <SpaceListAlt spaces={user.spaces} plants={plants} />
-      {/* <div>
-
-        <SearchPlants user={user} setUser={setUser}/>
-      </div> */}
     </>
   )
 }
