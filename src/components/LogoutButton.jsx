8ef24a10bda084705054f62146831217dce44170
kalfung <14322@coderacademy.edu.au>
2023-09-03 21:10:21 +1000

    added comments to LogoutButton component

--- src/components/LogoutButton.jsx
+++ src/components/LogoutButton.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { Link } from 'react-router-dom' 
 
 const style = { right: '1rem', textDecoration: 'none', color: 'green'}
+
+// Passes a user click event to clear the local storage of the user Bearer token and redirects the client to the login page
 const handleLogout = () => {
     localStorage.clear()
     return redirect('/login')
